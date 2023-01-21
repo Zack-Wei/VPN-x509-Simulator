16fe06e91d5e65b07414e4956343cdcce8da0d48
Zack-Wei <datongwei@foxmail.com>
2023-01-21 00:48:44 +0000

    v2.1. update the GPG sig

---
+++ GPG-backup/gpg.sh
@@ -0,0 +1,28 @@
+# help others to sign
+# import 
+gpg --homedir $home --import $file
+
+# get id
+gpg --homedir $home --list-key $name_of_someone
+
+# sign
+gpg --homedir $home --ask-cert-level --sign-keys $id
+
+# export 
+gpg --homedir $home --armor --output $someone.key.asc --export $id
+
+
+
+# import form others
+
+## import
+gpg --homedir $home --import $files_from_others
+
+## check signature which is signed by others
+gpg --homedir $home --list-signatures
+
+## uplopad 
+gpg --homedir $home --keyserver $key_server --send-key $your_id
+
+
+cdppass
