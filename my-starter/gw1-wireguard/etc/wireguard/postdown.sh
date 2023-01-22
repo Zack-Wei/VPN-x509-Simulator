92466e63f5875df6fae48b2f028f5a0d567d19a6
Zack-Wei <datongwei@foxmail.com>
2023-01-22 17:55:42 +0000

    v2.2. Update the pcap files

--- my-starter/gw1-wireguard/etc/wireguard/postdown.sh
+++ my-starter/gw1-wireguard/etc/wireguard/postdown.sh
@@ -6,5 +6,5 @@ iptables -D FORWARD -i wg0 -j ACCEPT
 iptables -D FORWARD -o wg0 -j ACCEPT
 
 # SNAT
-iptables -t nat -D POSTROUTING -o eth1 -j MASQUERADE
+#iptables -t nat -D POSTROUTING -o eth1 -j MASQUERADE
 
--- my-starter/gw1-wireguard/etc/wireguard/postup.sh
+++ my-starter/gw1-wireguard/etc/wireguard/postup.sh
@@ -6,4 +6,4 @@ iptables -A FORWARD -i wg0 -j ACCEPT
 iptables -A FORWARD -o wg0 -j ACCEPT
 
 # SNAT
-iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+#iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
